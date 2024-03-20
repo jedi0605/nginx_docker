0793b8297244463a40af6aca547784fc741c693b
jedi <jedi2454@gmail.com>
2024-03-20 10:58:43 -0400

    Two wordpress

---
+++ wordpress1/wp-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * WordPress Database Settings.
+ */
+
+define( 'DB_NAME', 'wordpress_db1' );
+define( 'DB_USER', 'wordpress_user1' );
+define( 'DB_PASSWORD', 'your_password1' );
+define( 'DB_HOST', 'mysql1' );
+define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_COLLATE', '' );
+$table_prefix = 'wp1_';
+
+// Other WordPress settings...
---
+++ wordpress2/wp-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * WordPress Database Settings.
+ */
+
+define( 'DB_NAME', 'wordpress_db2' );
+define( 'DB_USER', 'wordpress_user2' );
+define( 'DB_PASSWORD', 'your_password2' );
+define( 'DB_HOST', 'mysql2' );
+define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_COLLATE', '' );
+$table_prefix = 'wp2_';
+
+// Other WordPress settings...
